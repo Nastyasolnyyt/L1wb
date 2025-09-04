4655b5ac2dad66ae6f1818b9f60bcd6f2db7caec
Nastyasolnyyt <116307870+Nastyasolnyyt@users.noreply.github.com>
2025-09-04 15:12:32 +0300

    Update main.go

--- goprsctic/cmd/L1.2/main.go
+++ goprsctic/cmd/L1.2/main.go
@@ -4,14 +4,15 @@ import "fmt"
 
 func main() {
 	m := [5]int{2, 4, 6, 8, 10}
-	ch := make(chan int, len(m))
+	ch := make(chan int, len(m))//создаем канал
 	for i := 0; i < 5; i++ {
 		go func(n int) {
-			ch <- n * n
-		}(m[i])
+			ch <- n * n//записываем данные в канал
+		}(m[i])//замыкание
 	}
 
 	for i := 0; i < len(m); i++ {
-		fmt.Println(<-ch)
+		fmt.Println(<-ch)//выводим данные из канала
+	}
 	}
 }
